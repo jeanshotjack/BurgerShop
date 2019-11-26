d465a9f2c85cb272bc2ddb47dea296f5d7a6bb03
jeanshotjack <dshalenkova@gmail.com>
2019-11-25 20:59:32 -0500

    herkou db?

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+conection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
   password: "2019codecamp",
   database: "burgers_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
